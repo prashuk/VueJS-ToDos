7172b540b522e5ac2be5334f387a34d565d89fca
Prashuk Ajmera <prashuk.ajmera@gmail.com>
2020-06-18 15:19:25 -0500

    Initial commit

---
+++ src/components/Addtodo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <form @submit="addTodo">
+      <input type="text" v-model="title" name="title" placeholder="Add Todo..." />
+      <input type="submit" name="Submit" class="btn" />
+    </form>
+  </div>
+</template>
+
+<script>
+// import { v4 as uuidv4 } from "uuid";
+
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+      const newTodo = {
+        // id: uuidv4(),
+        title: this.title,
+        completed: false
+      };
+      this.$emit("add-todo", newTodo);
+      this.title = "";
+    }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+input[type="text"] {
+  float: 10;
+  padding: 5px;
+  width: 100%;
+}
+input[type="submit"] {
+  float: 2;
+}
+</style>
